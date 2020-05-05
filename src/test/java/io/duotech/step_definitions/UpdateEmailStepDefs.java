ff155ad13c9a2851674b4edfa6e36d28120b7008
duotechacademy <62758775+duotechacademy@users.noreply.github.com>
2020-05-05 03:44:58 -0400

    changed the password back

--- src/test/java/io/duotech/step_definitions/UpdateEmailStepDefs.java
+++ src/test/java/io/duotech/step_definitions/UpdateEmailStepDefs.java
@@ -20,7 +20,7 @@ public class UpdateEmailStepDefs {
 	public void i_log_in_as_a_user(String string) {
 		HomePage hp = new HomePage();
 		hp.loginUsername.sendKeys(string);
-		hp.loginPassword.sendKeys("akmalbekk");
+		hp.loginPassword.sendKeys("akmalbek");
 		hp.loginButton.click();
 
 
